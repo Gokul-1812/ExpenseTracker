b300e99ceeaa8de8d61ab671ffc5a02b00659405
Gokul-1812 <gokulvc1812@gmail.com>
2023-03-23 09:25:07 +0530

    added year filter

--- src/App.js
+++ src/App.js
@@ -1,46 +1,59 @@
 import Expenses from "./components/Expenses";
-import React from "react";
+import React, { useState }from "react";
 import NewExpense from "./components/NewExpense/NewExpense";
 
+const Dummyexpenses = [
+  {
+    id: "e1",
+    title: "Toilet Paper",
+    amount: 94.12,
+    date: new Date(2020, 7, 14),
+  },
+  { id: "e2", title: "New TV", amount: 799.49, date: new Date(2021, 2, 12) },
+  {
+    id: "e3",
+    title: "Car Insurance",
+    amount: 294.67,
+    date: new Date(2021, 2, 28),
+  },
+  {
+    id: "e4",
+    title: "New Desk (Wooden)",
+    amount: 450,
+    date: new Date(2021, 5, 12),
+  },
+];
+
 function App() {
-  const expenses = [
-    {
-      id: "e1",
-      title: "Toilet Paper",
-      amount: 94.12,
-      date: new Date(2020, 7, 14),
-    },
-    { id: "e2", title: "New TV", amount: 799.49, date: new Date(2021, 2, 12) },
-    {
-      id: "e3",
-      title: "Car Insurance",
-      amount: 294.67,
-      date: new Date(2021, 2, 28),
-    },
-    {
-      id: "e4",
-      title: "New Desk (Wooden)",
-      amount: 450,
-      date: new Date(2021, 5, 12),
-    },
-  ];
+
+  const [expense, setExpense]= useState(Dummyexpenses);
+
+
+  const addExpenseHandler = (expense) => {
+    setExpense((prevExpenses) => {
+      return [expense, ...prevExpenses];
+    });
+  };
+  const divStyle = {
+    margin: "60px",
+  };
 
   return (
-    // <div>
-    //   <h2>Expense Tracker</h2>
-    //   <Expenses items={expenses} />
-    // </div>
+    <div>
+      <h2 style={divStyle}>Expense Tracker</h2>
+      <NewExpense onAddExpense={addExpenseHandler}></NewExpense>
+      <Expenses items={expense} />
+    </div>
 
     // Alternative way for writting JSX code using React library
     // The JSX code written above will be converted to below code before compiling in browser
 
-
-    React.createElement(
-      "div",
-      {},
-      React.createElement(NewExpense, {},),
-      React.createElement(Expenses, { items: expenses })
-    )
+    // React.createElement(
+    //   "div",
+    //   {},
+    //   React.createElement(NewExpense, {},),
+    //   React.createElement(Expenses, { items: expenses })
+    // )
   );
 }
 
--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -1,24 +1,70 @@
 import "./ExpenseForm.css";
+import React, { useState } from "react";
 
-const ExpenseForm = () => {
+const ExpenseForm = (props) => {
+  const [enteredTitle, setTitle] = useState("");
+  const [enteredAmount, setAmount] = useState("");
+  const [enteredDate, setDate] = useState("");
+
+  const titleChangeHandler = (event) => {
+    setTitle(event.target.value);
+  };
+
+  const amountChangeHandler = (event) => {
+    setAmount(event.target.value);
+  };
+  const dateChangeHandler = (event) => {
+    setDate(event.target.value);
+  };
+
+  const submitHandler = (event) => {
+    event.preventDefault();
+
+    const expenseData = {
+      title: enteredTitle,
+      amount: enteredAmount,
+      date: new Date(enteredDate),
+    };
+    props.onSaveExpenese(expenseData);
+    setTitle("");
+    setAmount("");
+    setDate("");
+  };
+
   return (
-    <form>
+    <form onSubmit={submitHandler}>
       <div className="new-expense__controls">
         <div className="new-expense__control">
           <label>Title</label>
-          <input type="text" />
+          <input
+            type="text"
+            value={enteredTitle}
+            onChange={titleChangeHandler}
+          />
         </div>
         <div className="new-expense__control">
           <label>Amount</label>
-          <input type="number" min="0.01" step="0.01" />
+          <input
+            type="number"
+            min="0.01"
+            step="0.01"
+            value={enteredAmount}
+            onChange={amountChangeHandler}
+          />
         </div>
         <div className="new-expense__control">
           <label>Date</label>
-          <input type="date" min="2019-01-01" max="2022-12-3" />
+          <input
+            type="date"
+            min="2019-01-01"
+            max="2022-12-3"
+            value={enteredDate}
+            onChange={dateChangeHandler}
+          />
         </div>
       </div>
       <div>
-        <button type='sumbit'>Add Expense</button>
+        <button type="sumbit">Add Expense</button>
       </div>
     </form>
   );
